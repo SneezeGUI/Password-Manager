f8478ffb5378aff34a00e36fd47f0a12ec0c1003
SneezeGUI <sneeze@demented.cc>
2024-12-14 14:32:24 -0800

    error handling & json data format
    added compatibility with being packed into exe
    
    aswell as fixes and more

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from json import JSONDecodeError
 from tkinter import *
 import random
 import sys
 import os
 from tkinter import messagebox
+import json
 ###Constants
 FONT_NAME = "Courier"
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -22,22 +24,87 @@ def generate_pass():
     #copy to clipboard auto
     tk.clipboard_clear()
     tk.clipboard_append(final_pass)
-
 def clear():
     password_entry.delete(0, 'end')
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_login():
-    with open(data, 'a') as file:
-        website = website_entry.get()
-        user = user_entry.get()
-        password = password_entry.get()
 
-        is_ok = messagebox.askokcancel(title=website, message=f'Confirm Details: \nEmail: {user} \nPassword: {password}')
-        if is_ok:
-            file.writelines(f'{website} | {user} | {password}\n')
-            website_entry.delete(0,END)
-            user_entry.delete(0, END)
+    website = website_entry.get()
+    user = user_entry.get()
+    password = password_entry.get()
+    new_data = {
+        website: {
+                "email": user,
+                "password": password,
+             }
+         }
+##todo finish day 30 --- was here in video---
+    if len(website) == 0 or len(password) ==0:
+            messagebox.askokcancel(title='OOPS', message='Please be sure to fill out all fields first!')
+    else:
+        try:
+            with open(data_path, 'r') as data_file:
+                # json.dump(new_data, data_file, indent = 4 )
+                data = json.load(data_file)
+                data.update(new_data)
+
+        except FileNotFoundError:
+            with open(data_path, "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+
+        except JSONDecodeError:
+            with open(data_path, "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            # Updating old data with new data
+            data.update(new_data)
+
+            with open(data_path, "w") as data_file:
+                # Saving updated data
+                json.dump(data, data_file, indent=4)
+        finally:
+            website_entry.delete(0, END)
             password_entry.delete(0, END)
+
+
+
+#----------------------------SEARCH FUNCTION--------------------------#
+def search():
+    website = website_entry.get()
+    try:
+        with open(data_path) as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found.")
+    except JSONDecodeError:
+        messagebox.askokcancel(title='OOPS', message='Database is currently empty.\nPlease save atleast one login before using search.')
+
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}\n\nCopied to Clipboard in user:pass format.")
+            tk.clipboard_clear()
+            tk.clipboard_append(f'{email}:{password}')
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for {website} exists.")
+
+#     website = website_entry.get()
+#     if website == '':
+#         messagebox.askokcancel(title='Error', message=f'No Website Entered')
+#     with open(DATA, "r") as file:
+#         lines = file.readlines()
+#
+#         for line in lines:
+#
+#             elif website in line:
+#                 print(line)
+#                 messagebox.askokcancel(title=f'{website}', message=f'Logins Found: {line}')
+
+            # elif website:
+            #     no_login =messagebox.askokcancel(title='Error', message=f'No logins Found')
+            #     if no_login:
+            #         os.execl(sys.executable, sys.executable, *sys.argv)
 # ---------------------------- UI SETUP ------------------------------- #
 tk = Tk()
 tk.title('Password Manager - by SneezeGUI')
@@ -48,21 +115,28 @@ def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
     base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
     return os.path.join(base_path, relative_path)
+
 image_path = resource_path('logo/logo.png')
 logo = PhotoImage(file=image_path)
+
+data_path = resource_path('data/data.json')
 
 canvas = Canvas(width=200, height=200)
 canvas.grid(column=1,row=0, columnspan=2)
 canvas.create_image(100, 100,image=logo,)
 
-data ='data.txt'
+
+
+
+# data_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+# data = resource_path(image_path)
 
 #website
-
 website_label = Label(text='Website:', font=('Courier', 10, 'bold'))
 website_label.grid(column=0,row=1)
-website_entry = Entry(width=40,)
-website_entry.grid(column=1,row=1, columnspan=4)
+
+website_entry = Entry(width=21,)
+website_entry.grid(column=1,row=1,)
 website_entry.focus()
 
 #email/username
@@ -76,17 +150,16 @@ user_entry.grid(column=1,row=2,columnspan=4)
 password_label = Label(text='Password:', font=('Courier', 10, 'bold'))
 password_label.grid(column=0,row=3)
 
-
 password_entry = Entry(width=21)
 password_entry.grid(column=1,row=3)
 
 #buttons
 password_gen = Button(text='Generate Password',borderwidth=.5, highlightcolor='black',command=generate_pass)
 password_gen.grid(column=2,row=3,columnspan=3)
-
-
+# save
 add = Button(text='Add to Database',width=30, command=save_login)
 add.grid(column=1,row=4,columnspan=3)
-
-
+#search
+search = Button(text='Search the Database',borderwidth=.5, highlightcolor='black',command=search)
+search.grid(column=2,row=1)
 tk.mainloop()
